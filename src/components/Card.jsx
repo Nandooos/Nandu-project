43905a47b161675ab5596712c51642e0af99ab5f
Nandakishor <nandakishor850@gmail.com>
2026-03-19 20:54:22 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ title, description,image, buttonLabel }) => {
       backgroundColor: "#f8fafc",
       boxShadow: "0 4px 6px rgba(0,0,0,0.1)"
     }}>
-       <img src={image} alt={title} style={{ width: "100%", borderRadius: "6px" }} />
+       <img src={image} alt={title} style={{height: "180px", width: "100%", borderRadius: "6px" }} />
       <h3 style={{ margin: "1rem 0 0.5rem" }}>{title}</h3>
       <p style={{ fontSize: "0.9rem", color: "#475569" }}>{description}</p>
       <Button label={buttonLabel} onClick={() => alert(`Applying for ${title}`)} />
